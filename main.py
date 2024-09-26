c42198ce5e12bdc515657d5700fbd2262ada4de8
Cloutiera <cloutiera@educ.college-montreal.qc.ca>
2024-09-26 10:03:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,30 +4,41 @@ groupe 406
 jeu de devinette
 """
 
-
 from random import randint
+
 continuer = 1
-while (continuer == 1):
-    max = 1000
-    min = 0
-    nombre = randint(min, max)
+maximum = 100
+minimum = 0
+
+
+def bornes():
+    global minimum, maximum
+    minimum = int(input("Entrez la borne minimale\n"))
+    maximum = int(input("Entrez la borne maximale\n"))
+
+
+while continuer == 1:
+
+    bornes()
+    print(minimum, maximum)
+
+    nombre = randint(minimum, maximum)
     nbr_essais = 0
     reponse = 0
-    print("J'ai choisi un nombre entre %d et %d. À toi de le deviner" % (min, max))
-    while (reponse != nombre):
+    print("J'ai choisi un nombre entre %d et %d. À toi de le deviner" % (minimum, maximum))
 
+    while reponse != nombre:
         nbr_essais += 1
         reponse = int(input("Entrez votre réponse\n"))
-        #print(nombre)
+        # print(nombre)
         if reponse > nombre:
-            print("Mauvaise réponse, le nombre est plus petit que" , reponse)
+            print("Mauvaise réponse, le nombre est plus petit que", reponse)
         elif nombre > reponse:
-            print("Mauvaise réponse, le nombre est plus grand que" , reponse)
+            print("Mauvaise réponse, le nombre est plus grand que", reponse)
         elif nombre == reponse:
-            print("Bravo %d était la bonne réponse. Vous avez réussi en %d essais" %(reponse, nbr_essais))
+            print("Bravo %d était la bonne réponse. Vous avez réussi en %d essais" % (reponse, nbr_essais))
         else:
             print("nombre invalide")
     continuer = int(input("voulez-vous continuer\n1. oui\n2. non\n"))
     if continuer == 2:
         print("Au revoir.")
-
